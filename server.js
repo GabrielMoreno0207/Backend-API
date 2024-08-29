95b3e68e4dd00af899263e1b2a60a2becda8bb5d
Gabriel Moreno <gabrielmfrontend@hotmail.com>
2024-08-29 11:39:07 -0300

    api

---
+++ server.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+app.post('/usuarios', async (req,res) =>{
+       
+      await  prisma.user.create({  
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+        })
+
+       res.status(201).json()
+})
+
+app.get('/usuarios', async (req, res) => {
+
+    let filter = {}
+
+    if (req.query.name) {
+        filter = {
+            name: req.query.name,
+            email: req.query.email,
+            age: req.query.age
+        }
+    }
+
+    const users = await prisma.user.findMany({
+        where: filter
+    })
+
+    res.status(200).json(users)
+})
+
+
+app.put('/usuarios/:id', async (req,res) =>{
+       
+    
+
+    await  prisma.user.update({
+        where:{
+            id:  req.params.id
+        } , 
+          data: {
+              email: req.body.email,
+              name: req.body.name,
+              age: req.body.age
+          }
+      })
+
+     res.status(201).json()
+})
+
+app.delete('/usuarios/:id', async (req, res) =>{
+        await prisma.user.delete({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json({message: ' Usuário deletado com Sucesso!' })
+})
+
+app.listen(3000)
+
+
